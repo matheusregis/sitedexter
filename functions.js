dfed9f5d093d049d210fbbb800e9aabdc62defa2
MatheusRegis <matheustrai@gmail.com>
2022-04-12 03:01:25 -0300

    Adicionado Home, Pix, e RedesSociais

--- functions.js
+++ functions.js
@@ -1,33 +1,20 @@
-function dados() {
-  const email = document.getElementById("email").value;
-  validaEmail(email);
-  const password = document.getElementById("senha").value;
-  console.log(email, password);
-}
-
-function validaEmail(email) {
-  if (email) {
-    const isValid = email.includes("@gmail.com");
-    if (!isValid) {
-      alert("Email invalido");
-    }
-  }
-}
-function onHoverAnchor(id) {
-  let tagName = document.getElementById(`${id}`).text;
-
-  document.getElementById(
-    `img${tagName === "PIX" ? "Pix" : tagName}`
-  ).style.display = "flex";
-  document
-    .getElementById(`img${tagName === "PIX" ? "Pix" : tagName}`)
-    .classList.add("fade-in");
-}
-
-function offHoverAnchor(id) {
-  let tagName = document.getElementById(`${id}`).text;
-
-  document.getElementById(
-    `img${tagName === "PIX" ? "Pix" : tagName}`
-  ).style.display = "none";
-}
+$(document).ready(function () {
+  $(".header ul li a").mouseover((anchor) => {
+    $(`#${anchor.currentTarget.id}`)
+      .parent()
+      .prepend(
+        `<img style="display: flex" class='blood fade-in' src='assets/sangue.png' />`
+      );
+  });
+  $(".header ul li a").mouseout((anchor) => {
+    $(".header ul li img").remove();
+  });
+  $(".videos").click(() => {
+    console.log("aq");
+    $(".modalVideo").modal("show");
+  });
+  $(".clipes").click(() => {
+    console.log("aq");
+    $(".modalClipes").modal("show");
+  });
+});
