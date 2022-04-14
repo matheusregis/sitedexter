3eaca0575c87bf631825a6dea77761be97833107
MatheusRegis <matheustrai@gmail.com>
2022-04-13 23:10:22 -0300

    Adicionado Carousel dos videos

--- functions.js
+++ functions.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+  // colocar o mouse adiciona a imagem
   $(".header ul li a").mouseover((anchor) => {
     $(`#${anchor.currentTarget.id}`)
       .parent()
@@ -6,6 +7,7 @@ $(document).ready(function () {
         `<img style="display: flex" class='blood fade-in' src='assets/sangue.png' />`
       );
   });
+  // tirar o mouse remove a img
   $(".header ul li a").mouseout((anchor) => {
     $(".header ul li img").remove();
   });
